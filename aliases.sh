516d3ed34ae87de4d394bda97f35b83c01774bb9
Ezinwa Okpoechi <brainmaestro@outlook.com>
2018-09-21 11:54:38 +0200

    Add uuid alias

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,5 @@ alias json="python -m json.tool"
 alias v=vim
 alias dsize='du -sch * | sort -h'
 alias dsizeh='du -sch .[!.]* * | sort -h'
+alias uuid='python -c "import uuid; print(uuid.uuid4())"'
+
