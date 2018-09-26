0c45bfea0f39c1409f799c2e4ed596eedc62bfd1
Ezinwa Okpoechi <brainmaestro@outlook.com>
2018-09-26 12:26:55 +0200

    Add random hash alias

--- aliases.sh
+++ aliases.sh
@@ -2,5 +2,6 @@ alias json="python -m json.tool"
 alias v=vim
 alias dsize='du -sch * | sort -h'
 alias dsizeh='du -sch .[!.]* * | sort -h'
-alias uuid='python -c "import uuid; print(uuid.uuid4())"'
+alias uuid='python -c "import uuid; print(uuid.uuid4())" | tr -d "\n"'
+alias rhash='python -c "import uuid; print(uuid.uuid4().hex)" | tr -d "\n"'
 
